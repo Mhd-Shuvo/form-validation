20ab5f3df376ba571153d032bf02fd2f7de03203
Mhd-Shuvo <mehedishuvo333@gmail.com>
2020-12-23 16:14:38 +0600

    form validation

---
+++ form.js
@@ -0,0 +1,31 @@
+document.getElementById('infoSubmit').onclick = function(){
+    var userName = document.getElementById('name');
+    var email = document.getElementById('email');
+    var password = document.getElementById('password');
+
+    if(userName.value=='' )
+    {
+        document.getElementById('userName_msg').innerHTML="*this field is required";
+    }
+    else if(userName.value.length < 8){
+        document.getElementById('userName_msg').innerHTML="*user name must be 8 charachters";
+    }
+    if(email.value=='') {
+        document.getElementById('email_msg').innerHTML="*this field is required";
+    }
+    else if(!(email.value.includes('@') && email.value.includes('.'))) 
+    {
+        document.getElementById('email_msg').innerHTML="*invalid email"; 
+    }
+    if(password.value=='' )
+    {
+        document.getElementById('password_msg').innerHTML="*this field is required";
+    }
+    else if(!(email.value.includes('$') || email.value.includes('_'))) 
+    {
+        document.getElementById('password_msg').innerHTML="*atleast one super charachter needed";
+    }
+};
+    
+
+
